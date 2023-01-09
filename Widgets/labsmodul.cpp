314683abd6b7258cbe85c3d1a4e9d8bc81f8fa83
thomas_lucky13 <mkoul@mail.ru>
2023-01-09 18:00:03 +0400

    Labs view changes

--- Widgets/labsmodul.cpp
+++ Widgets/labsmodul.cpp
@@ -8,28 +8,24 @@ LabsModul::LabsModul(MainWindow* mMainWindow, QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QList<Theme> usedThems = mMainWindow->themsManager->getUsedThems();
-
-    for (int i = 0; i < usedThems.count(); ++i)
-    {
-        QLabel* themeLabel = new QLabel();
-        themeLabel->setText(usedThems[i].Name());
-        themeLabel->setStyleSheet("color: rgb(201, 209, 200);");
-        ui->themsLayout->addWidget(themeLabel);
-    }
-
+    tasksButtons = new QButtonGroup();
     QList<Task> tasks = mMainWindow->tasksManager->getTasks();
 
     for (int i = 0; i < tasks.count(); ++i)
     {
-        QLabel* taskLabel = new QLabel();
-        taskLabel->setText(tasks[i].Name());
-        taskLabel->setStyleSheet("color: rgb(201, 209, 200);");
-        ui->tasksLayout->addWidget(taskLabel);
+        QPushButton* button = new QPushButton();
+        button->setText("  "+tasks[i].Name());
+        button->setStyleSheet("text-align:left;\ncolor: rgb(201, 209, 200);");
+
+        tasksButtons->addButton(button, i);
+        ui->tasksLayout->addWidget(button);
     }
+    ui->tasksLayout->addSpacerItem(
+                new QSpacerItem(20,40, QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Expanding));
 
     connect(ui->themsEditButton, SIGNAL(clicked()), mMainWindow, SLOT(labsRedactionModulOn()));
     connect(ui->taskEditButton, SIGNAL(clicked()), mMainWindow, SLOT(labsTasksRedactionModulOn()));
+    connect(tasksButtons, SIGNAL(idClicked(int)), mMainWindow, SLOT(labsTaskViewModulOn(int)));
 }
 
 LabsModul::~LabsModul()
